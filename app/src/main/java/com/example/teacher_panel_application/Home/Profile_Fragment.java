4322a66a0a41ffe43e4943b4f30b0be2f95a0670
zeeshantaj <zeeshantaj.zt@gmail.com>
2024-02-29 19:32:37 +0500

    main ui  commit

--- app/src/main/java/com/example/teacher_panel_application/Home/Profile_Fragment.java
+++ app/src/main/java/com/example/teacher_panel_application/Home/Profile_Fragment.java
@@ -1,6 +1,20 @@
 package com.example.teacher_panel_application.Home;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import com.example.teacher_panel_application.R;
+
 
 public class Profile_Fragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_profile,container,false);
+    }
 }
