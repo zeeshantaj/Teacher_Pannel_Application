1482ec6ff2a2abad411dc1423f7aff40fecb2b71
zeeshantaj <zeeshantaj.zt@gmail.com>
2024-03-03 22:16:17 +0500

    main ui  commit

--- app/src/main/java/com/example/teacher_panel_application/Fragments/SignUp_Fragment.java
+++ app/src/main/java/com/example/teacher_panel_application/Fragments/SignUp_Fragment.java
@@ -27,52 +27,13 @@ import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link SignUp_Fragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class SignUp_Fragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public SignUp_Fragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SignUp_Fragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static SignUp_Fragment newInstance(String param1, String param2) {
-        SignUp_Fragment fragment = new SignUp_Fragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
     private View view;
     private TextView signUpText;
     private FrameLayout parentFrameLayout;
@@ -167,17 +128,7 @@ public class SignUp_Fragment extends Fragment {
                             dialog.dismiss();
                         }
                     });
-//                    auth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-//                        @Override
-//                        public void onComplete(@NonNull Task<AuthResult> task) {
-//
-//                        }
-//                    }).addOnFailureListener(new OnFailureListener() {
-//                        @Override
-//                        public void onFailure(@NonNull Exception e) {
-//
-//                        }
-//                    });
+
                 }
 
             }
