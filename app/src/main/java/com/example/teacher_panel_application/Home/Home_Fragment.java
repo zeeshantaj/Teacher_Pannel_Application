10970f396a4859e03c332fb2f727f409fabba7ae
zeeshantaj <zeeshantaj.zt@gmail.com>
2024-03-05 14:11:55 +0500

    main ui  commit

--- app/src/main/java/com/example/teacher_panel_application/Home/Home_Fragment.java
+++ app/src/main/java/com/example/teacher_panel_application/Home/Home_Fragment.java
@@ -24,6 +24,7 @@ import com.example.teacher_panel_application.Activities.Upload_Details_Activity;
 import com.example.teacher_panel_application.Fragments.EditDataFragment;
 import com.example.teacher_panel_application.Models.UploadClassModel;
 import com.example.teacher_panel_application.R;
+import com.example.teacher_panel_application.databinding.HomeFragmentBinding;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.MobileAds;
@@ -51,56 +52,43 @@ import java.util.Locale;
 
 public class Home_Fragment extends Fragment {
 
+    HomeFragmentBinding binding;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.home_fragment,container,false);
+        binding = HomeFragmentBinding.inflate(inflater,container,false);
+        //return inflater.inflate(R.layout.home_fragment,container,false);
+
+        return binding.getRoot();
     }
 
-    private TextView name,depart,subject,duration,locataion,topic,startedAt,timer,noClassTxt;
-    private CardView cardNode;
     private FirebaseDatabase database;
     private DatabaseReference reference;
     private FirebaseAuth auth;
     private CountDownTimer countDownTimer;
     private long timeRemainingInMillis,timeDifferenceMillis;
-
-    private AdView adView;
-    private CircularTimerView progressBar;
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        name = view.findViewById(R.id.nametxt);
-        depart = view.findViewById(R.id.departText);
-        subject = view.findViewById(R.id.subjectTxt);
-        duration = view.findViewById(R.id.durationTxt);
-        locataion = view.findViewById(R.id.locationTxt);
-        topic = view.findViewById(R.id.topopicTxt);
-        noClassTxt = view.findViewById(R.id.noClass);
-        startedAt = view.findViewById(R.id.startedTxt);
-        timer = view.findViewById(R.id.countTImer);
-        cardNode = view.findViewById(R.id.card_node);
-         progressBar = view.findViewById(R.id.progress_circular);
-        //getValues();
         MobileAds.initialize(getActivity(), new OnInitializationCompleteListener() {
             @Override
             public void onInitializationComplete(@NonNull InitializationStatus initializationStatus) {
                 Toast.makeText(getActivity(), " successful ", Toast.LENGTH_SHORT).show();
             }
         });
-        adView = view.findViewById(R.id.adView);
+//        adView = view.findViewById(R.id.adView);
 
         //adView.setAdUnitId("ca-app-pub-4144305165966580/3630690721");
         AdRequest adRequest = new AdRequest.Builder().build();
 
-        adView.loadAd(adRequest);
-        cardNode.setOnLongClickListener(view1 -> {
+        binding.adView.loadAd(adRequest);
+        binding.cardNode.setOnLongClickListener(view1 -> {
             showEditDataAlertDialog();
             return true;
         });
 
-        progressBar.setProgress(0);
-        progressBar.setCircularTimerListener(new CircularTimerListener() {
+        binding.progressCircular.setProgress(0);
+        binding.progressCircular.setCircularTimerListener(new CircularTimerListener() {
             @Override
             public String updateDataOnTick(long remainingTimeInMs) {
                 return String.valueOf((int)Math.ceil((remainingTimeInMs / 1000.f)));
@@ -109,13 +97,13 @@ public class Home_Fragment extends Fragment {
             @Override
             public void onTimerFinished() {
                 Toast.makeText(getActivity(), "FINISHED", Toast.LENGTH_SHORT).show();
-                progressBar.setPrefix("");
-                progressBar.setSuffix("");
-                progressBar.setText("FINISHED THANKS!");
+                binding.progressCircular.setPrefix("");
+                binding.progressCircular.setSuffix("");
+                binding.progressCircular.setText("FINISHED THANKS!");
             }
         }, 2, TimeFormatEnum.MINUTES, 10);
 
-        progressBar.startTimer();
+        binding.progressCircular.startTimer();
 
 // To Initialize Timer
 
@@ -132,31 +120,32 @@ public class Home_Fragment extends Fragment {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 if (snapshot.exists()) {
-                    noClassTxt.setVisibility(View.GONE);
-                    cardNode.setVisibility(View.VISIBLE);
-                    String name1 = snapshot.child("name").getValue(String.class);
+                    binding.cardNode.setVisibility(View.VISIBLE);
+                    binding.noClass.setVisibility(View.GONE);
+                    String name = snapshot.child("name").getValue(String.class);
                     String dep = snapshot.child("department").getValue(String.class);
                     String loc = snapshot.child("location").getValue(String.class);
                     String sub = snapshot.child("subject").getValue(String.class);
                     String topi = snapshot.child("topic").getValue(String.class);
-                    String minute1 = snapshot.child("minutes").getValue(String.class);
+                    String minute = snapshot.child("minutes").getValue(String.class);
                     String start = snapshot.child("currentTime").getValue(String.class);
                     String dateTime = snapshot.child("dateTime").getValue(String.class);
                     String endTimeString = snapshot.child("endTime").getValue(String.class);
-                    name.setText(name1);
-                    name.setSelected(true);
-                    duration.setText(minute1+" Minutes");
-                    duration.setSelected(true);
-                    depart.setText(dep);
-                    depart.setSelected(true);
-                    locataion.setText(loc);
-                    locataion.setSelected(true);
-                    subject.setText(sub);
-                    subject.setSelected(true);
-                    topic.setText(topi);
-                    topic.setSelected(true);
-                    startedAt.setText(start);
-                    startedAt.setSelected(true);
+
+                    binding.nametxt.setText(name);
+                    binding.nametxt.setSelected(true);
+                    binding.durationTxt.setText(minute+" minutes");
+                    binding.durationTxt.setSelected(true);
+                    binding.departText.setText(dep);
+                    binding.departText.setSelected(true);
+                    binding.locationTxt.setText(loc);
+                    binding.locationTxt.setSelected(true);
+                    binding.subjectTxt.setText(sub);
+                    binding.subjectTxt.setSelected(true);
+                    binding.topopicTxt.setText(topi);
+                    binding.topopicTxt.setSelected(true);
+                    binding.startedTxt.setText(start);
+                    binding.startedTxt.setSelected(true);
 
 
                     try {
@@ -185,22 +174,22 @@ public class Home_Fragment extends Fragment {
                                 timeRemainingInMillis = millisUntilFinished;
                                 updateCountdownText();
                                 if (millisUntilFinished < 50000){
-                                    timer.setTextColor(R.color.red);
+                                    binding.countTImer.setTextColor(R.color.red);
                                 }
                             }
                             @Override
                             public void onFinish() {
                                 // Perform any actions after the countdown finishes
 
-                                UploadClassModel uploadClassModel = new UploadClassModel(name1,dep,loc,sub,topi,minute1,dateTime);
+                                UploadClassModel uploadClassModel = new UploadClassModel(name,dep,loc,sub,topi,minute,dateTime);
                                 DatabaseReference addToQueueReference  = FirebaseDatabase.getInstance().getReference("PostedData").child(uid).child(dateTime);
                                 addToQueueReference.setValue(uploadClassModel).addOnCompleteListener(new OnCompleteListener<Void>() {
                                     @Override
                                     public void onComplete(@NonNull Task<Void> task) {
                                         if (task.isSuccessful()){
                                             reference.removeValue();
-                                            cardNode.setVisibility(View.GONE);
-                                            noClassTxt.setVisibility(View.VISIBLE);
+                                            binding.cardNode.setVisibility(View.GONE);
+                                            binding.noClass.setVisibility(View.VISIBLE);
                                         }
                                     }
                                 }).addOnFailureListener(new OnFailureListener() {
@@ -209,21 +198,15 @@ public class Home_Fragment extends Fragment {
                                         Toast.makeText(getActivity(), "Error "+e.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
                                     }
                                 });
-//                                Intent intent = new Intent(getActivity(), Upload_Details_Activity.class);
-//                                startActivity(intent);
-//                                getActivity().finish();
                             }
                         };
                         countDownTimer.start();
                         Log.e("countDownStarted place",countDownTimer.toString());
                     }
-
                 }
                 else {
-//                    Toast.makeText(getActivity(), "You have not posted class yet", Toast.LENGTH_SHORT).show();
-                    cardNode.setVisibility(View.GONE);
-                    noClassTxt.setVisibility(View.VISIBLE);
-
+                    binding.cardNode.setVisibility(View.GONE);
+                    binding.noClass.setVisibility(View.VISIBLE);
                 }
             }
 
@@ -239,7 +222,7 @@ public class Home_Fragment extends Fragment {
         int seconds = (int) (timeRemainingInMillis / 1000) % 60;
 
         String timeFormatted = String.format(Locale.US,"%02d:%02d:%02d", hours, minutes, seconds);
-        timer.setText(timeFormatted);
+        binding.countTImer.setText(timeFormatted);
     }
 
     private void showEditDataAlertDialog() {
