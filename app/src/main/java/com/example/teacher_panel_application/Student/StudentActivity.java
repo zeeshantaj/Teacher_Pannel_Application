af5b36b40bc4aca225c5b99223557164714e475c
zeeshantaj <zeeshantaj.zt@gmail.com>
2024-06-18 19:09:09 +0500

    adding new login features

---
+++ app/src/main/java/com/example/teacher_panel_application/Student/StudentActivity.java
@@ -0,0 +1,21 @@
+package com.example.teacher_panel_application.Student;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.teacher_panel_application.R;
+
+public class StudentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student);
+
+    }
+}
