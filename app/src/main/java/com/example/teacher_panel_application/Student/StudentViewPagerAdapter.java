b2dc498159c536ffae43b5e596111e51466968f3
zeeshantaj <zeeshantaj.zt@gmail.com>
2024-07-13 13:05:03 +0500

    student home ui

--- app/src/main/java/com/example/teacher_panel_application/Student/StudentViewPagerAdapter.java
+++ app/src/main/java/com/example/teacher_panel_application/Student/StudentViewPagerAdapter.java
@@ -6,18 +6,14 @@ import androidx.fragment.app.FragmentActivity;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 
 import com.example.teacher_panel_application.Home.History_Fragment;
-import com.example.teacher_panel_application.Home.Home_Fragment;
 import com.example.teacher_panel_application.Home.Settings_Fragment;
-import com.example.teacher_panel_application.Home.UploadDetails_Fragment;
 import com.example.teacher_panel_application.Student.fragments.StudentHome;
 
 public class StudentViewPagerAdapter extends FragmentStateAdapter {
     private static final int NUM_PAGES = 4;
-
     public StudentViewPagerAdapter(@NonNull FragmentActivity fragmentActivity) {
         super(fragmentActivity);
     }
-    
     @NonNull
     @Override
     public Fragment createFragment(int position) {
