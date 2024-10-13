5842d7594ce660da2fcdbfcd8a9ebbafa46b20e8
zeeshan <zeeshantaj.zt@gmail.com>
2024-10-13 22:01:25 +0500

    zego functionality

---
+++ app/src/main/java/com/example/teacher_panel_application/VideoStreaming/KeyConstants.java
@@ -0,0 +1,6 @@
+package com.example.teacher_panel_application.VideoStreaming;
+
+public class KeyConstants {
+    public static final String appSign = "";
+    public static final long appID = 123;
+}
